58a51b2d074daa7d2e97b75b409a507ed5e51f8b
Andy An <andyanappdev@gmail.com>
2023-12-11 17:18:40 +0900

    feat: Create InventoryViewController class

---
+++ JuiceMaker/JuiceMaker/Presentation/InventoryScreen/InventoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryViewController.swift
+//  JuiceMaker
+//
+//  Created by Doyoung An on 12/11/23.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
